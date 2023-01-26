48ccff4b303fecdd1c4e033de72ace1f45428044
4109 <mattapallisravya@gmail.com>
2023-01-25 19:32:36 -0600

    flask working

--- app.py
+++ app.py
@@ -40,7 +40,17 @@ def crime_data():
     
     for post in posts:
         crime_income_data = {}
-        crime_income_data["state"] = post[0]
+        crime_income_data["FIPS"] = post[0]
+        crime_income_data["county"] = post[1]
+        crime_income_data["state"] = post[2]
+        crime_income_data["crime_rate_rank"] = post[4]
+        crime_income_data["total_violent_crime"] = post[14]
+        crime_income_data["household_income_rank"] = post[15]
+        crime_income_data["per_capita_income"] = post[17]
+        crime_income_data["lat"] = post[21]
+        crime_income_data["lng"] = post[22]
+
+
         data.append(crime_income_data)
 
 
