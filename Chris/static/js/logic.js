8e3652803ec10d744d1cc9428fffebf54b493140
cjs0428 <114039517+cjs0428@users.noreply.github.com>
2023-01-26 21:43:44 -0600

    updates

---
+++ Chris/static/js/logic.js
@@ -0,0 +1,57 @@
+// Creating the map object
+let countyLine= L.layerGroup();
+
+let stateLine= L.layerGroup(); 
+
+// Adding the tile layer
+let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+
+// let countyGeo = ('https://eric.clst.org/assets/wiki/uploads/Stuff/gz_2010_us_050_00_500k.json');
+let countyGeo = 'https://raw.githubusercontent.com/kjhealy/us-county/master/data/geojson/gz_2010_us_050_00_500k.json';
+
+let stateGeo = 'https://raw.githubusercontent.com/kjhealy/us-county/master/data/geojson/gz_2010_us_040_00_500k.json';
+
+d3.json(countyGeo).then(data => {
+  console.log(data);
+  L.geoJSON(data, {
+    color: "black",
+    weight: 1
+  }).addTo(countyLine);
+  // countyLine.addTo(myMap);
+});
+
+d3.json(stateGeo).then(data2 => {
+  console.log(data2);
+  L.geoJSON(data2, {
+    color: "darkblue",
+    weight: .7
+  }).addTo(stateLine);
+  // stateLine.addTo(myMap);
+});
+
+let baseMaps = {
+  'Street': street,
+  'Topo': topo
+};
+
+let overlayMap = {
+  'County Line': countyLine,
+  'State Line': stateLine,
+};
+
+let myMap = L.map("map", {
+  center: [37.7749, -100.4194],
+  zoom: 4.5,
+  layers: [topo, stateLine]
+});
+
+L.control.layers(baseMaps, overlayMap, {
+  collapsed: false
+}).addTo(myMap);
+
